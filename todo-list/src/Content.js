b6c0a58c4841d51efee7834c162ef60560a6743d
Vasanthkumar M <vasanth23402@gmail.com>
2024-06-14 17:21:19 +0530

    Event Listener has implemented

--- todo-list/src/Content.js
+++ todo-list/src/Content.js
@@ -2,10 +2,22 @@ import React from 'react'
 import './Content.css'
 
 function Content() {
+
+  function handleClick() {
+    let todoDiv = document.getElementById("todo-msgs")
+    let input = document.getElementById("todo-msg")
+    let para = `<p id="msg">${input.value}</p>`
+    todoDiv.innerHTML += para
+  }
+
+  function handleDblClick() {
+    console.log("I am Came by double click")
+  }
+
   return (
     <div id='content'>
         <input type="text" id="todo-msg"/>
-        <button id='btn'>Add</button>
+        <button id='btn' onClick={() => handleClick()} onDoubleClick={() => handleDblClick()}>Add</button>
         <div id="todo-msgs">
     
         </div>
