d04576c49847aafc9d5e9ff5ce7ef8de7137d956
Vasanthkumar M <vasanth23402@gmail.com>
2024-06-27 14:11:29 +0530

    useRef hook has implemented for auto focus after clicking a button

--- todo-list-app/src/AddItem.js
+++ todo-list-app/src/AddItem.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './AddItem.css'
 import { FaPlus } from 'react-icons/fa'
+import { useRef } from 'react'
 
 export default function AddItem({addItem, setAddItem, handleSubmit}) {
+
+  let inputRef = useRef()
+
   return (
     <form id="form" onSubmit={handleSubmit}>
         <label id="addItem" htmlFor="addItem">Add Item</label>
         <input 
+            ref={inputRef}
             id='add-item'
             type="text"
             placeholder='Add Item'
@@ -14,7 +19,7 @@ export default function AddItem({addItem, setAddItem, handleSubmit}) {
             value = {addItem}
             onChange = {(e) => setAddItem(e.target.value)}
         />
-        <button id="item-button" type='submit' aria-label='Add Item'>
+        <button id="item-button" type='submit' aria-label='Add Item' onClick={() => inputRef.current.focus()}>
             <FaPlus
                 role= 'button'
             />
